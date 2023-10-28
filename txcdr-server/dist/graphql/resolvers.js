21150cb7928cc3d00165e11482d99a885927129d
FuRy30 <adam.jan.duda.krk@gmail.com>
2023-10-28 22:57:27 +0200

    event creation and update

--- txcdr-server/dist/graphql/resolvers.js
+++ txcdr-server/dist/graphql/resolvers.js
@@ -5,5 +5,36 @@ export const resolvers = {
         getUsers: () => {
             return prisma.user.findMany();
         }
+    },
+    Mutation: {
+        createEvent: async (_, { input }) => {
+            try {
+                const event = await prisma.event.create({
+                    data: {
+                        ...input
+                    }
+                });
+
+                return event;
+            } catch (error) {
+                throw new Error(`Failed to create event: ${error.message}`);
+            }
+        },
+        updateEvent: async (_, { input }) => {
+            try {
+                const event = await prisma.event.update({
+                    where: {
+                        id: input.id,
+                    },
+                    data: {
+                        ...input
+                    }
+                });
+
+                return event;
+            } catch (error) {
+                throw new Error(`Failed to create event: ${error.message}`);
+            }
+        }
     }
 };
