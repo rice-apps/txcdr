6b9c41eecced31bd87a1406f32b48fa0ba1029cc
Gabriel <gabrielong2004@gmail.com>
2023-10-28 13:08:01 -0500

    Set up backend

---
+++ txcdr-server/prisma/migrations/20231022192551_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
---
+++ txcdr-server/prisma/migrations/20231022192640_test/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "users";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
