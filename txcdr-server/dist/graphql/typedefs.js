578de28a1cc0088cd5b6e6d59ec0e0e36fb42d7b
FuRy30 <adam.jan.duda.krk@gmail.com>
2023-10-28 22:57:27 +0200

    event creation and update

--- txcdr-server/dist/graphql/typedefs.js
+++ txcdr-server/dist/graphql/typedefs.js
@@ -5,7 +5,21 @@ export const typeDefs = `
         name: String
     }
 
+    type Event {
+        id: ID!,
+        isActive: Boolean!,
+        location: String!,
+        description: String,
+        title: String!,
+        nHouses: Int!
+    }
+
     type Query {
         getUsers: [User!]!
+    }
+
+    type Mutation {
+        createEvent(input: EventInput!): Event!,
+        updateEvent(input: EventInput!): Event!
     }
 `;
