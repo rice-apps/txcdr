9aded52b950d26fd9ea10a1ff21dea6f175c942b
Ovikx <ovikdas10@gmail.com>
2023-11-04 15:22:19 -0500

    add base map to map page

--- txcdr-client/app/map/index.tsx
+++ txcdr-client/app/map/index.tsx
@@ -1,12 +1,14 @@
 import { Link } from "expo-router";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import MapView from "react-native-maps";
 
 export default function Page() {
   return (
     <SafeAreaView>
       <Text className="text-2xl">Map page</Text>
       <Link href="/">Go back to home page</Link>
+      <MapView className="w-full h-full" />
     </SafeAreaView>
   );
 }
