337fc70b06948e5d118e3a24b61df6c6ff32bfc3
Ovikx <ovikdas10@gmail.com>
2023-11-18 16:37:04 -0600

    add docs for ISvgProps

--- txcdr-client/types/utils.tsx
+++ txcdr-client/types/utils.tsx
@@ -1,5 +1,8 @@
 import { SvgProps } from "react-native-svg";
 
+/**
+ * Extended interface for SVGProps for React Native compatibility
+ */
 export interface ISvgProps extends SvgProps {
   xmlns?: string;
   xmlnsXlink?: string;
