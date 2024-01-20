bdac2b2905c7b2e3bbab0b77d34f8d1af318405c
Ovikx <ovikdas10@gmail.com>
2024-01-20 14:59:16 -0600

    add sign out button

--- txcdr-client/app/(auth)/profile/index.tsx
+++ txcdr-client/app/(auth)/profile/index.tsx
@@ -1,13 +1,25 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
+import { useSession } from "../../../auth/ctx";
+import { router } from "expo-router";
 
 /**
  * The volunteer profile page
  * @returns Profile page component
  */
 export default function Page() {
+  const { signOut } = useSession();
   return (
     <View>
       <Text>Profile page</Text>
+      <Pressable
+        className="px-4 py-2 bg-slate-400"
+        onPress={() => {
+          signOut();
+          router.replace("/");
+        }}
+      >
+        <Text>Sign out</Text>
+      </Pressable>
     </View>
   );
 }
