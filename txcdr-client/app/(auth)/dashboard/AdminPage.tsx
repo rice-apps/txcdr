be433c11a14a285a6ed888a303f0df9b08e715a3
Ovik <ovikdas10@gmail.com>
2024-07-03 00:11:30 -0400

    add conditional rendering and role fetching hook

---
+++ txcdr-client/app/(auth)/dashboard/AdminPage.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { DText } from "../../../components/styled-rn/DText";
+
+export function AdminPage() {
+  return (
+    <View>
+      <DText>Admin dashboard</DText>
+    </View>
+  );
+}
