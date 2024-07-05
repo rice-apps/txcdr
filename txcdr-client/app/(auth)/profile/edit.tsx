fa616d151b06f88762b1fda97c9e92fee8805897
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2024-07-04 19:06:21 -0500

    Install eas and modify app to support profile

---
+++ txcdr-client/app/(auth)/profile/edit.tsx
@@ -0,0 +1,209 @@
+import { View, Text, ScrollView, TextInput, Image } from "react-native";
+import { useForm, Controller } from "react-hook-form";
+import { Pressable } from "react-native";
+import * as ImagePicker from "react-native-image-picker";
+import { useState } from "react";
+import { router } from "expo-router";
+
+// Edit page form for user profile, where users can update their information
+export default function Page() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      pronouns: "",
+      age: "",
+      languages: "",
+      organizations: "",
+    },
+  });
+  const [image, setImage] = useState<string | null>(null);
+
+  const onCameraSelect = async () => {
+    console.log("selecting camera");
+    const result = await ImagePicker.launchImageLibrary({
+      mediaType: "photo",
+      includeBase64: true,
+    });
+    if (result.assets) {
+      setImage(result.assets[0].base64 || null);
+    }
+  };
+
+  return (
+    <ScrollView className="p-8 flex flex-col">
+      <View>
+        <Text className="text-3xl font-semibold">Edit Profile</Text>
+      </View>
+      <View className="flex flex-col pt-5 justify-center align-middle self-center">
+        <Image
+          height={75}
+          width={75}
+          className="rounded-full"
+          source={{ uri: image || "https://reactnative.dev/img/tiny_logo.png" }}
+        />
+        <Pressable className="pt-3" onPress={onCameraSelect}>
+          <Text className="text-center text-lg text-blue-500 font-medium">
+            Edit
+          </Text>
+        </Pressable>
+      </View>
+      {/* Form Sections */}
+      <View className="flex flex-col pt-5 pb-6">
+        <Text className="text-blue-600 text-2xl font-semibold pb-2">
+          Personal Information
+        </Text>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View className="pb-4">
+              <Text className="text-xl pb-2 font-medium">First Name</Text>
+              <TextInput
+                placeholder="First Name"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                className="border border-gray-300 rounded-3xl p-4 w-full"
+              />
+            </View>
+          )}
+          name="firstName"
+        />
+
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <>
+              <Text className="text-xl pb-2 font-medium">Last Name</Text>
+              <TextInput
+                placeholder="Last Name"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                className="border border-gray-300 rounded-3xl p-4 w-full"
+              />
+            </>
+          )}
+          name="lastName"
+        />
+      </View>
+      <View className="flex flex-col pt-5 pb-6">
+        <Text className="text-blue-600 text-2xl font-semibold pb-2">
+          Demographic Information
+        </Text>
+        <View className="flex flex-row">
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <View className="flex-1">
+                <Text className="text-xl pb-2 font-medium">Pronouns</Text>
+                <TextInput
+                  placeholder="Pronouns"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  className="border border-gray-300 rounded-3xl p-4 w-full"
+                />
+              </View>
+            )}
+            name="pronouns"
+          />
+          <View className="px-6"></View>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <View className="flex-1">
+                <Text className="text-xl pb-2 font-medium">Age</Text>
+                <TextInput
+                  numberOfLines={1}
+                  placeholder="Age"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  className="border border-gray-300 rounded-3xl p-4 w-full"
+                  keyboardType="numeric"
+                />
+              </View>
+            )}
+            name="age"
+          />
+        </View>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View className="pt-4">
+              <Text className="text-xl pb-2 font-medium">Languages Spoken</Text>
+              <TextInput
+                placeholder="Languages Spoken"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                multiline={true}
+                className="border border-gray-300 rounded-3xl p-4 w-full h-32"
+              />
+            </View>
+          )}
+          name="languages"
+        />
+      </View>
+      <View className="flex flex-col pt-5 pb-6">
+        <Text className="text-blue-600 text-2xl font-semibold pb-2">
+          Organizations
+        </Text>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <>
+              <TextInput
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                multiline={true}
+                className="border border-gray-300 rounded-3xl p-4 w-full h-32"
+              />
+            </>
+          )}
+          name="organizations"
+        />
+      </View>
+      <View className="flex flex-row gap-x-12">
+        <Pressable
+          className="rounded-3xl py-4 border-2 border-gray-400 flex-1"
+          onPress={() => router.back()}
+        >
+          <Text className="text-center text-lg font-semibold">Cancel</Text>
+        </Pressable>
+        <Pressable
+          className="bg-blue-500 rounded-3xl py-4 flex-1"
+          onPress={() => router.back()}
+        >
+          <Text className="text-white text-center text-lg font-semibold">
+            Save Changes
+          </Text>
+        </Pressable>
+      </View>
+    </ScrollView>
+  );
+}
---
+++ txcdr-client/app/(auth)/profile/index.tsx
@@ -0,0 +1,182 @@
+import {
+  Pressable,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
+import { useSession } from "../../../auth/ctx";
+import { supabase } from "../../../utils/supabase";
+import { useEffect, useState } from "react";
+import { Tables } from "../../../types/supabase";
+import { Icon } from "@rneui/themed/dist/Icon";
+import { router } from "expo-router";
+
+/**
+ * The volunteer profile page
+ * @returns Profile page component
+ */
+export default function Page() {
+  const { signOut, session } = useSession();
+  const [user, setUser] = useState<Tables<"User2"> | null>(null);
+  // Get the user's profile data using useEffect from supabase
+  useEffect(() => {
+    // get our own id
+    supabase.auth.getUser().then((res) => {
+      // we should definitely be logged in
+      let id = res.data.user!.id;
+
+      supabase
+        .from("User2")
+        .select()
+        .eq("id", id)
+        .then((res) => {
+          setUser(res.data![0]);
+        });
+    });
+  });
+  return (
+    <ScrollView
+      contentContainerStyle={
+        !user ? { flexGrow: 1, justifyContent: "center" } : {}
+      }
+    >
+      {!user && (
+        <View className="self-center justify-self-center">
+          <ActivityIndicator size="large"></ActivityIndicator>
+        </View>
+      )}
+      {user && (
+        <View className="flex flex-col px-10 py-5 justify-center align-middle gap-y-6">
+          {/* Profile page */}
+          <View className="flex flex-row justify-start gap-x-5">
+            <Image
+              height={50}
+              width={50}
+              className="rounded-full"
+              source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }}
+            />
+            <Text className="self-center text-2xl mr-auto">Admin Name</Text>
+            <Icon
+              name={"edit"}
+              size={20}
+              style={{ alignSelf: "center", marginVertical: "auto" }}
+              onPress={() => {
+                router.push("/profile/edit");
+              }}
+            ></Icon>
+          </View>
+          {/* Loading spinner if user is not loaded */}
+          {/* Display user data */}
+          {
+            <View className="gap-y-4">
+              <View className="drop-shadow-2xl">
+                <View className="bg-blue-500 py-4 px-8 rounded-t-3xl drop-shadow-2xl">
+                  <Text className="text-white text-xl">
+                    Contact Information
+                  </Text>
+                </View>
+                <View className="bg-slate-300 px-10 pt-6 pb-8 rounded-b-3xl drop-shadow-2xl">
+                  {/* Phone */}
+                  <View className="flex flex-col gap-y-2">
+                    <Text className="text-sm uppercase">Phone</Text>
+                    <Text>{user.phone}</Text>
+                  </View>
+                  {/* Email */}
+                  <View className="flex flex-col gap-y-2">
+                    <Text className="text-sm uppercase">Email</Text>
+                    <Text>{user.email}</Text>
+                  </View>
+                </View>
+              </View>
+              <View className="drop-shadow-2xl">
+                <View className="bg-blue-500 py-4 px-8 rounded-t-3xl drop-shadow-2xl">
+                  <Text className="text-white text-xl">
+                    Demographic Information
+                  </Text>
+                </View>
+                <View className="bg-slate-300 px-10 pt-6 pb-8 rounded-b-3xl drop-shadow-2xl">
+                  <View className="flex flex-col w-full gap-y-4">
+                    {/* Phone */}
+                    <View className="flex flex-row gap-x-10">
+                      <Text className="text-sm uppercase w-25 mr-auto">
+                        Phone
+                      </Text>
+                      <Text className="text-sm">8328664850</Text>
+                    </View>
+                    {/* Email */}
+                    <View className="flex flex-row gap-x-10">
+                      <Text className="text-sm uppercase mr-auto">Email</Text>
+                      <Text className="text-sm">{user.email}</Text>
+                    </View>
+                    {/* Email */}
+                    <View className="flex flex-row gap-x-10">
+                      <Text className="text-sm uppercase mr-auto">Email</Text>
+                      <Text className="text-sm">{user.email}</Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+              <View className="drop-shadow-2xl">
+                <View className="bg-blue-500 py-4 px-8 rounded-t-3xl drop-shadow-2xl">
+                  <Text className="text-white text-xl">Organizations</Text>
+                </View>
+                <View className="bg-slate-300 px-10 pt-6 pb-8 rounded-b-3xl drop-shadow-2xl">
+                  {/* Phone */}
+                  <View className="flex flex-col gap-y-2">
+                    <Text className="text-sm uppercase">Phone</Text>
+                    <Text>{user.phone}</Text>
+                  </View>
+                  {/* Email */}
+                  <View className="flex flex-col gap-y-2">
+                    <Text className="text-sm uppercase">Email</Text>
+                    <Text>{user.email}</Text>
+                  </View>
+                </View>
+              </View>
+              <View className="drop-shadow-2xl">
+                <View className="bg-blue-500 py-4 px-8 rounded-t-3xl drop-shadow-2xl">
+                  <Text className="text-white text-xl">
+                    Contact Information
+                  </Text>
+                </View>
+                <View className="bg-slate-300 px-10 pt-6 pb-8 rounded-b-3xl drop-shadow-2xl">
+                  {/* Phone */}
+                  <View className="flex flex-col gap-y-2">
+                    <Text className="text-sm uppercase">Phone</Text>
+                    <Text>{user.phone}</Text>
+                  </View>
+                  {/* Email */}
+                  <View className="flex flex-col gap-y-2">
+                    <Text className="text-sm uppercase">Email</Text>
+                    <Text>{user.email}</Text>
+                  </View>
+                </View>
+              </View>
+              <View className="drop-shadow-2xl">
+                <View className="bg-blue-500 py-4 px-8 rounded-t-3xl drop-shadow-2xl">
+                  <Text className="text-white text-xl">
+                    Contact Information
+                  </Text>
+                </View>
+                <View className="bg-slate-300 px-10 pt-6 pb-8 rounded-b-3xl drop-shadow-2xl">
+                  {/* Phone */}
+                  <View className="flex flex-col gap-y-2">
+                    <Text className="text-sm uppercase">Phone</Text>
+                    <Text>{user.phone}</Text>
+                  </View>
+                  {/* Email */}
+                  <View className="flex flex-col gap-y-2">
+                    <Text className="text-sm uppercase">Email</Text>
+                    <Text>{user.email}</Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+          }
+        </View>
+      )}
+    </ScrollView>
+  );
+}
