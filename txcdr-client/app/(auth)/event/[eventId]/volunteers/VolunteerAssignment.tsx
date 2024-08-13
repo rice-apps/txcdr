469731073cbc998d7ecb1eb537134b7231e590a1
Shivam Pathak <58636209+pathaks1@users.noreply.github.com>
2024-08-12 22:34:23 -0700

    initial block id selector

--- txcdr-client/app/(auth)/event/[eventId]/volunteers/VolunteerAssignment.tsx
+++ txcdr-client/app/(auth)/event/[eventId]/volunteers/VolunteerAssignment.tsx
@@ -4,18 +4,49 @@ import { ms } from "react-native-size-matters";
 import { VolunteerDetails } from "./VolunteerDetails";
 import { DText } from "../../../../../components/styled-rn/DText";
 import { Blue } from "../../../../../utils/styles/colors";
+import { useEffect, useState } from "react";
+import { supabase } from "../../../../../utils/supabase";
+import { Picker } from "@react-native-picker/picker";
 
 interface Props {
   eventId: string;
   user: User;
 }
 
-export function VolunteerAssignment({ user }: Props) {
+export function VolunteerAssignment({ user, eventId }: Props) {
+  const [blockIds, setBlockIds] = useState([])
+  const [selectedId, setSelectedId] = useState("Select")
+
+  useEffect(() => {
+    let eventIdNum = Number(eventId)
+    const func = async () => {
+      const { data, error } = await supabase.rpc("find_block_ids", {eventidnum: eventIdNum})
+      if (error) {
+        throw error
+      }
+      setBlockIds(data)
+    }
+    func()
+  }, [])
   return (
     <View style={styles.container}>
       <VolunteerDetails name={user.name} org={user.organizations} />
       <Pressable style={[styles.button, { backgroundColor: Blue[500] }]}>
-        <DText style={{ color: "white" }}>Assign block</DText>
+        <Picker selectedValue={selectedId}
+          mode="dropdown"
+          style={styles.picker}
+          onValueChange={async (itemValue, itemIndex) => {
+            setSelectedId(itemValue)
+            const {error} = await supabase
+              .from("EventVolunteer")
+              .update({ blockId: itemValue })
+              .eq("volunteerId", user.id)
+              .eq("eventId", Number(eventId))
+            if (error) throw error
+          }}>
+            {blockIds.map((item, index) => 
+            {return (<Picker.Item label={item} value={item} key={index}/>)})}
+          </Picker>
       </Pressable>
     </View>
   );
@@ -32,5 +63,9 @@ const styles = StyleSheet.create({
     borderRadius: ms(10),
     paddingHorizontal: ms(10),
     paddingVertical: ms(5),
+  },
+  picker: {
+    height: ms(50),
+    width: ms(100),
   },
 });
