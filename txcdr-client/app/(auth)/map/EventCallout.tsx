c529c28f26e9b5f258d5db470ed5f09f0173f5c7
Ovikx <ovikdas10@gmail.com>
2024-10-15 01:21:21 -0500

    make map marker callout redirect to address page on press

--- txcdr-client/app/(auth)/map/EventCallout.tsx
+++ txcdr-client/app/(auth)/map/EventCallout.tsx
@@ -2,6 +2,7 @@ import { Pressable, SafeAreaView, Text, View } from "react-native";
 import { Callout } from "react-native-maps";
 import { EventMarker } from "../../../types/map";
 import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
 
 interface Props {
   eventData: EventMarker;
@@ -12,10 +13,10 @@ interface Props {
  * @param param0 Event data
  * @returns React component
  */
-export function StatusName({ text } : { text: String }) {
+export function StatusName({ text }: { text: String }) {
   return (
     <View className="self-start bg-orange-300 rounded-xl">
-      <Text className="text-sm self-start px-2">{ text }</Text>
+      <Text className="text-sm self-start px-2">{text}</Text>
     </View>
   );
 }
@@ -27,7 +28,9 @@ export function EventCallout({ eventData }: Props) {
         <Text className="text-lg font-bold">{eventData.title}</Text>
         <StatusName text={"Status"}></StatusName>
         <View className="self-end flex flex-row items-center">
-          <Text className="pl-1 text-gray-700">{"more"}</Text>
+          <Pressable onPress={() => router.navigate(`/list/${eventData.id}`)}>
+            <Text className="pl-1 text-gray-700">{"more"}</Text>
+          </Pressable>
           <Ionicons name="chevron-forward"></Ionicons>
         </View>
       </SafeAreaView>
